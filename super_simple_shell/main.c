716fc4fbfadc32043a377a54343da5369eb37d96
Ignacio Castro <ignaciocastro347@gmail.com>
2022-07-27 13:03:13 -0700

    Create split function to separate string by delimiters

--- super_simple_shell/main.c
+++ super_simple_shell/main.c
@@ -1,30 +1,68 @@
+#define  _GNU_SOURCE
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <string.h>
+int split(char ***, char *, char *);
 
 /**
  * main - executes the command ls -l /tmp in 5 different child processes
  *
  * Return: Always 0.
  */
-int main(int argc, char **argv)
+int main()
 {
-	(void) argc;
 	int status;
+	size_t len = 0;
+	char *buffer = 0;
+	char *delim = " \t\n";
+	char **tempArgs;
 
-	if (fork() == 0)
+	printf("#cisfun$ ");
+	while ((getline(&buffer, &len, stdin)) != -1)
 	{
-		printf("From child\n");
-		if (execve(argv[0], argv, NULL) == -1)
+		/* take first token */
+		split(&tempArgs, buffer, delim);
+		printf("%s", tempArgs[0]);
+		/* create a child process */
+		if (tempArgs && tempArgs[0] && fork() == 0)
 		{
-			perror("Error:");
+			/* exec shell command, return error message if it fails */
+			if (execve(tempArgs[0], tempArgs, NULL) == -1)
+				perror("Error:");
+			break;
+		}
+		else
+		{
+			/* wait until child process ends to continue */
+			wait(&status);
+			printf("#cisfun$ ");
 		}
 	}
-	else
+	return (0);
+}
+
+int split(char ***dest, char *buffer, char *delim)
+{
+	char **list;
+	char *token = 0;
+	int i = -1;
+
+	token = strtok(buffer, delim);
+	while (token)
 	{
-		wait(&status);
-		printf("end of child\n");
+		i++;
+		list = realloc(list, sizeof(char) * i);
+		if (!dest)
+			return (-1);
+		list[i] = malloc(sizeof(char) * strlen(token));
+		if (list[i])
+			return(-1);
+		list[i] = token;
+		token = strtok(NULL, delim);
 	}
+	**dest = *list;
 	return (0);
 }
---
+++ super_simple_shell/split.c
@@ -0,0 +1,33 @@
+#include "shell.h"
+/**
+ * split - create an array of strings from a string separated by chars
+ * @buffer: string value to delimit
+ * @delim: string with chars which ar delimiters
+ * Return: array of string
+ */
+char **split(char *buffer, char *delim)
+{
+	char **list = NULL;
+	char *token = 0;
+	char *dbuffer;
+	int i = -1;
+
+	if (!dest || !buffer)
+		return (-1);
+	dbuffer = strdup(buffer);
+	token = strtok(dbuffer, delim);
+	while (token)
+	{
+		i++;
+		list = realloc(list, sizeof(char *) * (i + 1));
+		if (!list)
+			return (NULL);
+		list[i] = strdup(token);
+		token = strtok(NULL, delim);
+	}
+	i++;
+	list = realloc(list, sizeof(char *) * (i + 1));
+	list[i] = NULL;
+	free(dbuffer);
+	return (list);
+}
