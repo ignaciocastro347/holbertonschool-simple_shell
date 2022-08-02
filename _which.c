fafa5c5379df02d53526df3cdad838a1542cd248
Jose Alvarez <5135@holbertonschool.com>
2022-08-01 19:37:08 -0700

    add _which function without valgrind errors

---
+++ _which.c
@@ -0,0 +1,19 @@
+#include "shell.h"
+#include "_string.h"
+extern char **environ;
+
+char *_which (char *str)
+{
+	int i = 0;
+	char *key = NULL;
+
+	for(i = 0; environ[i]; i++)
+	{
+		key = strtok(environ[i], "=");
+		if(strcmp(key, str) == 0)
+		{
+			return(strtok(NULL, "="));
+		}
+	}
+	return (NULL);
+}
