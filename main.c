36cf90d1db8004f72c9ec1d4ecfe77f33df3e75b
José Alvarez <jol.alvarezc@gmail.com>
2022-08-04 20:17:23 -0300

    comeback (#21)

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
  */
 int main()
 {
-	int int_mode = 0, status = 0;
+	int int_mode = 0;
 	size_t len = 0;
 	char *buffer = 0, *delim = " \t\n", **args = 0, *command = NULL;
 	ssize_t getline_status = 1;
@@ -24,29 +24,35 @@ int main()
 		command = strtok(buffer, "\n");
 		if (!command)
 			continue;
+		/*if (!buffer[0])
+		{
+			free(buffer);
+			continue;
+		}*/
 		args = split(command, delim);
 		free(buffer);
 		buffer = NULL;
 
-		status = execute_program(args);
+		execute_program(args);
 		free_string_list(args);
-		if (status != 0)
-			return(status);
 	}
 	return (0);
 }
-int execute_program(char **args)
+void execute_program(char **args)
 {
-	int status = 0, wait_status;
+	int status;
 	char *tmp = NULL, *paths = NULL;
+
 	if (!args || !args[0])
-		return (status);
+		return;
 
 	if (args[0][0] == '/' || args[0][0] == '.')
 	{
 		if (access(args[0], F_OK) != 0)
 		{
-			return (print_not_found(args[0]));
+			print_not_found(args[0]);
+			free_string_list(args);
+			exit(127);
 		}
 	}
 	else
@@ -55,22 +61,17 @@ int execute_program(char **args)
 		paths = _get_env("PATH");
 		if (!paths)
 		{
-			if (access(args[0], F_OK) != 0)
-			{
-				status = print_not_found(tmp);
-			}
+			print_not_found(tmp);
 			free_string_list(args);
-			return(status);
+			exit(127);
 		}
 		args[0] = _which(paths, args[0]);
 		if (!args[0])
 		{
-			if (access(args[0], F_OK) != 0)
-			{
-				status = print_not_found(tmp);
-			}
+			print_not_found(tmp);
+			free_string_list(args);
 			free(tmp);
-			return(status);
+			return;
 		}
 		free(tmp);
 	}
@@ -81,14 +82,12 @@ int execute_program(char **args)
 		free_string_list(args);
 	}
 	else
-		wait(&wait_status);
-	return (0);
+		wait(&status);
 }
 
-int print_not_found(char *cmd)
+void print_not_found(char *cmd)
 {
 	write(STDERR_FILENO, "./hsh: 1: ", 10);
 	write(STDERR_FILENO, cmd, _strlen(cmd));
 	write(STDERR_FILENO, ": not found\n", 12);
-	return (127);
 }
