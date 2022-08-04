d5be6434b0f81ee256941dae02fa093787965403
Jose Alvarez <5135@holbertonschool.com>
2022-08-04 05:54:35 -0700

    WIP - Clean code

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include "shell.h"
 #include "_printf.h"
-
 /**
  * main - shell
  * Return: Always 0.
@@ -39,39 +38,41 @@ void execute_program(char *buffer, char **args)
 	int status;
 	char *tmp = NULL;
 
-	if (args && args[0])
-	{
-		/* Check if program name already starts with '/' */
-		if (args[0][0] == '/')
-		{
-			if (access(args[0], F_OK) != 0)
-			{
-				_printf("This program does not exist!!!");
-				return;
-			}
-		}
-		else
-		{
-			/* Get full path of program and set it into args[0] */
-			tmp = args[0];
-			args[0] = _which(_get_env("PATH"), args[0]);
-			if (!args[0])
-			{
-				_printf("Which no match\n");
-				return;
-			}
-			free(tmp);
-		}
+	if (!args || !args[0])
+		return;
 
-		/* fork process and execute program */
-		if (fork() == 0)
+	if (args[0][0] == '/')
+	{
+		if (access(args[0], F_OK) != 0)
 		{
-			if (execve(args[0], args, environ) == -1)
-				perror("Error:");
-			free_string_list(args);
-			free(buffer);
+			write(STDERR_FILENO, "./hsh", 1);
+			write(STDERR_FILENO, ": 1: ", 1);
+			write(STDERR_FILENO, args[0], 1);
+			write(STDERR_FILENO, ": not found", 1);
+			return;
 		}
-		else
-			wait(&status);
 	}
+	else
+	{
+		tmp = args[0];
+		args[0] = _which(_get_env("PATH"), args[0]);
+		if (!args[0])
+		{
+			write(STDERR_FILENO, "./hsh", 1);
+ 			write(STDERR_FILENO, ": 1: ", 1);
+			write(STDERR_FILENO, args[0], 1);
+ 			write(STDERR_FILENO, ": not found", 1);
+			return;
+		}
+		free(tmp);
+	}
+	if (fork() == 0)
+	{
+		if (execve(args[0], args, environ) == -1)
+			perror("Error:");
+		free_string_list(args);
+		free(buffer);
+	}
+	else
+		wait(&status);
 }
